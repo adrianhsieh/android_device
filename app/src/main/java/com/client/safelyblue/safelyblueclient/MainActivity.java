a927170982b77fef9c6635e67b861478df31000e
Albin Kjellin <albin.kjellin@gmail.com>
2015-03-23 07:33:01 -0700

    Updated frequency.

--- app/src/main/java/com/client/safelyblue/safelyblueclient/MainActivity.java
+++ app/src/main/java/com/client/safelyblue/safelyblueclient/MainActivity.java
@@ -1,5 +1,6 @@
 package com.client.safelyblue.safelyblueclient;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
@@ -20,11 +21,11 @@ import com.client.safelyblue.safelyblueclient.db.PositionReportDB;
 import java.lang.Override;
 import java.lang.String;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements LocationListener {
 
     private static final String TAG = "MainActivity";
 
-    private LocationListener locListener = new MyLocationListener();
+    //private LocationListener locListener = new MyLocationListener();
     private static String imei;
 
 
@@ -42,55 +43,89 @@ public class MainActivity extends ActionBarActivity {
         startService(startPositionSyncServiceIntent);
 
         LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000, 0, locListener);
-
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 1, this);
+        Log.i(TAG,"Is GPS Enabled? "+locManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
 
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG,"Location Changed"+location);
 
 
+        if(location != null) {
+            ContentValues locationMap = new ContentValues();
+            locationMap.put("longitude", location.getLongitude());
+            locationMap.put("latitude", location.getLatitude());
+            locationMap.put("altitiude", location.getAltitude());
+            locationMap.put("accuracy", location.getAltitude());
+            locationMap.put("datetimestamp", location.getTime());
+
+
+            pDB = new PositionReportDB(MainActivity.this);
+            boolean dbInsertResult = pDB.insertPosition(locationMap);
+
+        }else{
+            Log.e(TAG, "No Location Created");
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.i(TAG, "Status Changed");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.i(TAG, "Provider Enabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.e(TAG, "Provider Disabled");
+    }
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "On Resume Called");
+
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "On Pause Called");
+
+    }
+*/
+/*
     class MyLocationListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
-            Log.i(TAG,"Location Changed"+location);
 
-
-            if(location != null) {
-                ContentValues locationMap = new ContentValues();
-                locationMap.put("longitude", location.getLongitude());
-                locationMap.put("latitude", location.getLatitude());
-                locationMap.put("altitiude", location.getAltitude());
-                locationMap.put("accuracy", location.getAltitude());
-                locationMap.put("datetimestamp", location.getTime());
-
-
-                pDB = new PositionReportDB(MainActivity.this);
-                boolean dbInsertResult = pDB.insertPosition(locationMap);
-
-            }else{
-                Log.e(TAG, "No Location Created");
-            }
 
 
         }
 
         @Override
         public void onProviderDisabled(String provider) {
-            // TODO Auto-generated method stub
+            Log.e(TAG, "Provider Disabled");
 
         }
 
         @Override
         public void onProviderEnabled(String provider) {
-            // TODO Auto-generated method stub
+            Log.i(TAG, "Provider Enabled");
 
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-            // TODO Auto-generated method stub
+            Log.e(TAG, "Provider Enabled");
 
         }
     }
+    */
 }
